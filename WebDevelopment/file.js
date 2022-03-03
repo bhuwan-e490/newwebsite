3b4e15cb4ca51205e395808f9dff965bc8b163e6
Bhuwan sharma <89209422+bhuwan-e490@users.noreply.github.com>
2022-03-03 18:01:40 +0545

    Add files via upload

---
+++ WebDevelopment/file.js
@@ -0,0 +1,33 @@
+// /////CLASSLIST/////
+
+// // // document.querySelector(".logo").classList.add("dell","lenovo","mac");
+// // document.querySelector(".logo").classList.remove("khiladi");
+// // const ram = document.querySelector(".logo").classList;
+
+// // // console.log(ram);
+// // const ram = document.querySelector(".logo").classList.toggle("khiladi");
+// // console.log(ram);
+
+// // const element = document.createElement("p");
+
+// // const Text = document.createTextNode("This looks interesting");
+
+
+
+// // element.appendChild(Text);
+// // document.querySelector(".wrapper").appendChild(element);
+// // console.log(element);
+
+
+// // const target = document.querySelector(".wrapper");
+// // target.insertBefore(element,target.childNodes[1]);
+
+// const create = document.createElement("p");
+// console.log(create);
+
+// const text  = document.createTextNode("War is stupid");
+// console.log(text);
+
+// create.appendChild(text);
+
+// document.querySelector(".logo").appendChild(creat
---
+++ WebDevelopment/file1.js
@@ -0,0 +1,15 @@
+function not(){
+    document.write("Be Strong !🇳🇵 ");
+    for (let count = 0; count < 300; "<br>" + count++ ) {
+    document.write("Be Strong! 🇳🇵" );
+         
+    }
+}
+
+function yes(){
+
+    document.write("You are not done yet!🚀" );
+    for (let count = 0;count<200;count++){
+        document.write("You are not done yet!🚀")
+    }
+}
